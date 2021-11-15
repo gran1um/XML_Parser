8cc48b8b36f7a1c78f6d5c5cf2449e5b5df7afc1
gran1um <spiderfan02@gmail.com>
2021-11-15 17:00:45 +0300

    1.0

--- xml.php
+++ xml.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-$xml_file = "tss1.xml";
+$xml_file = "tss_new.xml";
 
 if (file_exists($xml_file)) {
     $xml = simplexml_load_file($xml_file);
@@ -22,7 +22,7 @@ if (file_exists($xml_file)) {
         };
 
         $sql = "delete from category WHERE (`id` = ".$row["id"]."); insert into category (id,  category, parent_id) values(".$row["id"].", '".$segment."', '".$str."');";
-        echo($sql);
+
         $fp=fopen("./cat.txt","a");   
         fwrite($fp, "\r\n" . $sql);  
         fclose($fp);
@@ -33,20 +33,40 @@ if (file_exists($xml_file)) {
     foreach ($xml->xpath("//offers/offer") as $segment) {
         $row = $segment->attributes();
         $str=str_replace('https://tss.ru/catalog/','',$segment->url);
-        $sql = "delete from goods WHERE (`id` = ".$row["id"]."); insert into goods (id, URL, available, cost,cost2, category, image , name, ext-description) values(".$row["id"].", '".$str."', ".$row["available"].", '".$segment->price." ₽"."', '".$segment->price."', ".$segment->categoryId.", '".$segment->picture."', '".$segment->name."', '".$segment->description."');";
+        $str= substr($str, 0, -1);
+        $description="";
+        $specifications="";
+
+        foreach($segment->param as $key=>$param){
+
+                if ($param["name"]=='Описание товара'){
+                    $description=$param;
+                }
+
+                if ($param['name']!='Артикул' & $param['name']!='Картинки'& $param['name']!='Детальное описание товара'& $param['name']!='Описание товара'){
+                    $specifications.="<tr><td>".$param['name']."</td><td>".$param."</td></tr>";
+                }
+                
+            }
+
+        
+        $specifications='<tbody><tr><td colspan="«2»" class="«table-part»"> <b> Заводские данные </b> </td></tr>'.$specifications.'</tbody>';
+
+        $sql = "delete from goods WHERE (`id` = ".$row["id"]."); insert into goods (id, URL, available, cost,cost2, category, image , name, description, `ext-description`,spec) values(".$row["id"].", '".$str."', ".$row["available"].", '".$segment->price." ₽"."', '".$segment->price."', ".$segment->categoryId.", '".$segment->picture."', '".$segment->name."', '".$segment->description."', '".$description."', '".$specifications."');";
 
         $path="";
         foreach($segment->picture as $key=>$picture){
-            $path+=$picture.":";
+            $path.=$picture."|";
             
         }
         $path= substr($path, 0, -1);
-        $sql1 = "delete from images WHERE (`URL` = ".$str."); insert into images (URL, path) values(".$str."', ".$path." ');";
+        
+        $sql1 = "delete from images WHERE (`goods_URL` = '".$str."'); insert into images (goods_URL, path) values('".$str."', '".$path."');";
+        $result=$sql.$sql1;
 
-        echo($sql."\r\n");
-        echo($sql1."\r\n");
         $fp=fopen("./goods.txt","a");  
-        fwrite($fp, "\r\n" . $sql);  
+        fwrite($fp, "\r\n" . $sql); 
+        fwrite($fp, "\r\n" . $sql1);  
         fclose($fp);
     
 }
